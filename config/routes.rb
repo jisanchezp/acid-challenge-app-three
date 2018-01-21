28703661dadb8280f62f943e1732a4c1d2238e13
jisanchezp <juanisanchez@outlook.com>
2018-01-21 17:59:34 -0300

    Clean routes and GemFile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   namespace :rest do
     get 'sessions/login'
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
